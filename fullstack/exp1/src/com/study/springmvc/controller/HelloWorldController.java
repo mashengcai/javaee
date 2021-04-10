808b056faf9675dc58876449dce137cc9b7fdc03
chulujian@126.com <chulujian@126.com>
2021-04-10 17:26:48 +0800

    third commit

---
+++ fullstack/exp1/src/com/study/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.study.springmvc.controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@org.springframework.stereotype.Controller
+public class HelloWorldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        // 创建模型
+        ModelAndView modelAndView = new ModelAndView();
+
+        // 向模型中添加数据
+        modelAndView.addObject("message", "Hello World");
+
+        // 返回逻辑视图
+        modelAndView.setViewName("/WEB-INF/jsp/helloWorld.jsp");
+        return modelAndView;
+    }
+}
